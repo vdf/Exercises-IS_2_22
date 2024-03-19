a157dd943b3724cce6fec40b988a4ac9c64f24d5
vdf <vasily.v.d@gmail.com>
2024-03-19 07:02:12 +0500

    Add files via upload

---
+++ Exercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Engine eng = new Engine();
+            Console.Out.WriteLine( "State: " + eng.getState() );
+            Console.Out.WriteLine();
+
+            eng.start();
+            Console.Out.WriteLine( "State: " + eng.getState() );
+            Thread.Sleep( 2000 );
+            Console.Out.WriteLine();
+
+            eng.stop();
+            Console.Out.WriteLine( "State: " + eng.getState() );
+
+            Console.ReadKey();
+        }
+    }
+}
