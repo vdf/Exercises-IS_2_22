0e5d35909fd7bcdb3ac7125aac0421ea9dae64d9
Stepan Stankevich <auazze.music@gmail.com>
2024-03-28 14:33:30 +0500

    Updated and Fixed Code

---
+++ Exercises-IS_2_22/Exercise1/Exercise1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Engine
+    {
+        private const int StateStopped = 1;
+        private const int StateStarted = 2;
+        public int engineState;
+
+        public void Start()
+        {
+            Console.Out.WriteLine( "Start" );
+            engineState = StateStarted;
+        }
+
+        public void Stop()
+        {
+            Console.Out.WriteLine( "Stop" );
+            engineState = StateStopped;
+        }
+
+        public string GetState()
+        {
+            string state = "WHROOM-WHROOM";
+
+            if(engineState == StateStopped)
+            {
+                state = "STOPPED";
+            }
+            else if(engineState == StateStarted)
+            {
+                state = "STARTED";
+            }
+
+            return state;
+        }
+    }
+}
---
+++ Exercises-IS_2_22/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Engine eng = new Engine();
+            Console.Out.WriteLine( "State: " + eng.GetState() );
+            Console.Out.WriteLine();
+
+            eng.Start();
+            Console.Out.WriteLine( "State: " + eng.GetState() );
+            Console.WriteLine("\nSleeping for 2 sec");
+            Thread.Sleep( 2000 );
+            Console.Out.WriteLine();
+
+            eng.Start();
+            Console.Out.WriteLine( "State: " + eng.GetState() );
+
+            Console.ReadKey();
+        }
+    }
+}
