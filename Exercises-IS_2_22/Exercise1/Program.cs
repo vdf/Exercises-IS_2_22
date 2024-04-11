f87cdadea743d37daa324110e8cc5a9063c0a443
Stepan Stankevich <auazze.music@gmail.com>
2024-04-11 14:15:03 +0500

    Changed classes

--- Exercises-IS_2_22/Exercise1/Program.cs
+++ Exercises-IS_2_22/Exercise1/Program.cs
@@ -23,18 +23,24 @@ namespace Exercise1
             Console.WriteLine();
 
             FuelTank tank = new FuelTank();
+            Console.WriteLine("Max tank volume is - " + tank.MaxVolume);
+            Console.WriteLine();
+
             Console.WriteLine("How much fuel you want to add ? (int)");
             int fuelToAdd = int.Parse(Console.ReadLine());
             tank.Refuel(fuelToAdd);
 
             Console.WriteLine("Fuel added. Current level is - " + tank.Fuel);
+            Console.WriteLine();
 
             Console.WriteLine("Enter the distance you driven (double)");
             string distanceDriven = Console.ReadLine().Replace(',', '.');
             double X = double.Parse(distanceDriven, CultureInfo.InvariantCulture);
             tank.ConsumeFuel(X);
+            Console.WriteLine();
 
             Console.WriteLine("After driving fuel level is - " + tank.Fuel);
+            Console.WriteLine();
 
             if (tank.IsFuelLow())
             {
